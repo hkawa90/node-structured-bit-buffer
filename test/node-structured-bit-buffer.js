48590a57a79021c40005897d2ecd626528169507
Kawamura Hideo <kawa90@yahoo.co.jp>
2016-03-06 21:25:15 +0900

    change  a bit

--- test/node-structured-bit-buffer.js
+++ test/node-structured-bit-buffer.js
@@ -24,7 +24,6 @@ describe('Structured Bit Buffer Module Test', function() {
             expect(bb.offset).to.equal(0);
             expect(bb.length).to.equal(0);
             bb = new BitBuffer(buffer);
-            expect(bb.buffer).to.equal(buffer);
             expect(bb.offset).to.equal(0);
             expect(bb.length).to.equal(buffer.length * 8);
             bb = new BitBuffer('1010');
@@ -48,18 +47,20 @@ describe('Structured Bit Buffer Module Test', function() {
             bb = new BitBuffer(buffer);
             expect(bb.toString()).to.equal('000000000000000000000000000000000000000000000000');
             buffer[0] = 0xff;
+            bb = new BitBuffer(buffer);
             expect(bb.toString()).to.equal('000000000000000000000000000000000000000011111111');
             for (var i = 0; i < 6; i++) {
                 buffer[i] = 0xff;
             }
+            bb = new BitBuffer(buffer);
             expect(bb.toString()).to.equal('111111111111111111111111111111111111111111111111');
             expect(bb.toString.bind(20), Error, 'Unspported radix.');
             bb = new BitBuffer('101');
             expect(bb.toString()).to.equal('101');
         });
         it('getBit method Test', function() {
-            bb = new BitBuffer(buffer);
             buffer[0] = parseInt('10101010', 2);
+            bb = new BitBuffer(buffer);
             expect(bb.getBit(0)).to.equal(0);
             expect(bb.getBit(1)).to.equal(1);
             expect(bb.getBit(2)).to.equal(0);
@@ -72,26 +73,31 @@ describe('Structured Bit Buffer Module Test', function() {
         it('setBit method Test', function() {
             bb = new BitBuffer(buffer);
             bb.setBit(0);
-            expect(buffer[0]).to.equal(1);
+            expect(bb.toString()).to.equal('000000000000000000000000000000000000000000000001');
             bb.setBit(2);
-            expect(buffer[0]).to.equal(5);
+            expect(bb.toString()).to.equal('000000000000000000000000000000000000000000000101');
             bb.setBit(4);
-            expect(buffer[0]).to.equal(21);
+            expect(bb.toString()).to.equal('000000000000000000000000000000000000000000010101');
             bb.setBit(6);
-            expect(buffer[0]).to.equal(85);
+            expect(bb.toString()).to.equal('000000000000000000000000000000000000000001010101');
         });
         it('and method Test', function() {
             // no extension
             var b1 = new Buffer(1);
             var b2 = new Buffer(1);
+
+            b1[0] = b2[1] = 0;
             var bb1 = new BitBuffer(b1);
             var bb2 = new BitBuffer(b2);
-            b1[0] = b2[1] = 0;
             expect(bb1.and(bb2).toString()).to.equal('00000000');
             b1[0] = b2[1] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.and(bb2).toString()).to.equal('11111111');
             b1[0] = 0;
             b2[1] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.and(bb2).toString()).to.equal('00000000');
             expect(bb1.and('11111111').toString()).to.equal('00000000');
             b1 = new Buffer(1);
@@ -103,93 +109,118 @@ describe('Structured Bit Buffer Module Test', function() {
         it('xor method Test', function() {
             var b1 = new Buffer(1);
             var b2 = new Buffer(1);
+
+            b1[0] = b2[0] = 0;
             var bb1 = new BitBuffer(b1);
             var bb2 = new BitBuffer(b2);
-            b1[0] = b2[0] = 0;
             expect(bb1.xor(bb2).toString()).to.equal('00000000');
             b1[0] = b2[0] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.xor(bb2).toString()).to.equal('00000000');
             b1[0] = 0;
             b2[0] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.xor(bb2).toString()).to.equal('11111111');
         });
         it('not method Test', function() {
             var b1 = new Buffer(1);
-            var bb1 = new BitBuffer(b1);
             b1[0] = 0;
+            var bb1 = new BitBuffer(b1);
             expect(bb1.not().toString()).to.equal('11111111');
             b1[0] = 0xff;
+            bb1 = new BitBuffer(b1);
             expect(bb1.not().toString()).to.equal('00000000');
             b1[0] = 170; // 10101010
+            bb1 = new BitBuffer(b1);
             expect(bb1.not().toString()).to.equal('01010101');
         });
         it('or method Test', function() {
             var b1 = new Buffer(1);
             var b2 = new Buffer(1);
+            b1[0] = b2[0] = 0;
             var bb1 = new BitBuffer(b1);
             var bb2 = new BitBuffer(b2);
-            b1[0] = b2[0] = 0;
             expect(bb1.or(bb2).toString()).to.equal('00000000');
             b1[0] = b2[0] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.or(bb2).toString()).to.equal('11111111');
             b1[0] = 0;
             b2[0] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.or(bb2).toString()).to.equal('11111111');
             b1[0] = 170; // 10101010
             b2[0] = 85; // 01010101
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.or(bb2).toString()).to.equal('11111111');
         });
         it('equal method Test', function() {
             var b1 = new Buffer(1);
             var b2 = new Buffer(1);
+            b1[0] = b2[0] = 0;
             var bb1 = new BitBuffer(b1);
             var bb2 = new BitBuffer(b2);
-            b1[0] = b2[0] = 0;
             expect(bb1.equal(bb2)).to.be.true;
             b1[0] = b2[0] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.equal(bb2)).to.be.true;
             b1[0] = 0;
             b2[0] = 0xff;
+            bb1 = new BitBuffer(b1);
+            bb2 = new BitBuffer(b2);
             expect(bb1.equal(bb2)).to.be.false;
         });
         it('reset method Test', function() {
             var b1 = new Buffer(1);
-            var bb1 = new BitBuffer(b1);
             b1[0] = 0;
+            var bb1 = new BitBuffer(b1);
             expect(bb1.reset().toString()).to.equal('00000000');
             b1[0] = 0xff;
+            bb1 = new BitBuffer(b1);
             expect(bb1.reset().toString()).to.equal('00000000');
             b1[0] = 170; // 10101010
+            bb1 = new BitBuffer(b1);
             expect(bb1.reset().toString()).to.equal('00000000');
         });
         it('any method Test', function() {
             var b1 = new Buffer(1);
-            var bb1 = new BitBuffer(b1);
             b1[0] = 0;
+            var bb1 = new BitBuffer(b1);
             expect(bb1.any()).to.be.false;
             b1[0] = 0xff;
+            bb1 = new BitBuffer(b1);
             expect(bb1.any()).to.be.true;
             b1[0] = 170; // 10101010
+            bb1 = new BitBuffer(b1);
             expect(bb1.any()).to.be.true;
         });
         it('count method Test', function() {
             var b1 = new Buffer(1);
-            var bb1 = new BitBuffer(b1);
             b1[0] = 0;
+            var bb1 = new BitBuffer(b1);
             expect(bb1.count()).to.equal(0);
             b1[0] = 0xff;
+            bb1 = new BitBuffer(b1);
             expect(bb1.count()).to.equal(8);
             b1[0] = 170; // 10101010
+            bb1 = new BitBuffer(b1);
             expect(bb1.count()).to.equal(4);
         });
         it('none method Test', function() {
             var b1 = new Buffer(1);
-            var bb1 = new BitBuffer(b1);
             b1[0] = 0;
+            var bb1 = new BitBuffer(b1);
             expect(bb1.none()).to.be.true;
             b1[0] = 0xff;
+            bb1 = new BitBuffer(b1);
             expect(bb1.none()).to.be.false;
             b1[0] = 170; // 10101010
+            bb1 = new BitBuffer(b1);
             expect(bb1.none()).to.be.false;
         });
         it('shift method Test', function() {
